476cfd2487b10eb1f50ddd0c1fc69a0ea882edfa
sidney zhang <sidneyzsc@gmail.com>
2013-01-17 18:43:02 +0800

    weather second commit

--- src/main/java/com/mytest/weather/YahooParser.java
+++ src/main/java/com/mytest/weather/YahooParser.java
@@ -24,22 +24,22 @@ public class YahooParser {
         Document doc = xmlReader.read(inputStream);
 
         log.info("Parsing XML Response");
-        weather.setCity(doc.valueOf("/rss/channel/y:location/@city"));
-        weather.setRegion(doc.valueOf("/rss/channel/y:location/@region"));
-        weather.setCountry(doc.valueOf("/rss/channel/y:location/@country"));
+        weather.setCity(doc.valueOf("/rss/channel/yweather:location/@city"));
+        weather.setRegion(doc.valueOf("/rss/channel/yweather:location/@region"));
+        weather.setCountry(doc.valueOf("/rss/channel/yweather:location/@country"));
 
-        weather.setCondition(doc.valueOf("/rss/channel/item/y:condition/@text"));
-        weather.setTemp(doc.valueOf("/rss/channel/item/y:condition/@temp"));
-        weather.setChill(doc.valueOf("/rss/channel/y:wind/@chill"));
+        weather.setCondition(doc.valueOf("/rss/channel/item/yweather:condition/@text"));
+        weather.setTemp(doc.valueOf("/rss/channel/item/yweather:condition/@temp"));
+        weather.setChill(doc.valueOf("/rss/channel/yweather:wind/@chill"));
 
-        weather.setHumidity(doc.valueOf("/rss/channel/y:atmosphere/@humidity"));
+        weather.setHumidity(doc.valueOf("/rss/channel/yweather:atmosphere/@humidity"));
 
         return weather;
     }
 
     private SAXReader createXmlReader() {
         Map<String, String> uris = new HashMap<String, String>();
-        uris.put("y", "http://xml.weather.yahoo.com/ns/rss/1.0");
+        uris.put("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
 
         DocumentFactory factory = new DocumentFactory();
         factory.setXPathNamespaceURIs(uris);
